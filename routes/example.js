8ccbfc3c03c6236787e9365b59f7dbd162719a79
BeoCrusader <crucifiedcrusader@gmail.com>
2016-09-23 08:40:54 +0300

    re-init jquery-ui. Example added

--- routes/example.js
+++ routes/example.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('/');
+router.get('/example', function(req, res, next) {
+  res.render('jqexample');
 });
 
 module.exports = router;
